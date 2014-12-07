d07dc77517e1e74e5436f86c6a966a7f860aa147
asshurimjoylarita <asshurim@cjnetsolutions.com>
2014-12-07 21:58:17 +0800

    Creating Controller Response

--- app/controllers/PaymentResponseController.php
+++ app/controllers/PaymentResponseController.php
@@ -9,12 +9,12 @@
 
 		public function handle()
 		{
-			$payment_response = new PaymentResponse();
-
-			$payment_response->data = serialize(Input::all());
-			$payment_response->entry_id = Input::get('entry_id');
-
-			$payment_response->save();
+			PaymentResponse::create([
+				
+				'data' => serialize(['name'=>'ash']),
+				'entry_id' => 1,
+				
+			]);
 
 			return;
 		}
