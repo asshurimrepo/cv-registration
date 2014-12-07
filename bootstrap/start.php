c5bf6ff228d9d17d3cb49601ff52519f84ab5b4d
asshurimjoylarita <asshurim@cjnetsolutions.com>
2014-12-07 17:46:54 +0800

    Creating Controller Response

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -27,8 +27,8 @@ $app = new Illuminate\Foundation\Application;
 $env = $app->detectEnvironment(array(
 
 	'local' => array('homestead'),
-	'production' => array('alfareedajos.com'),
 	'preview' => array('cjnetsolutions.com'),
+	'production' => array('alfareedajos.com'),
 
 ));
 
