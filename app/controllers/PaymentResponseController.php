7a1fb783d5ce8f0baef0b766fafa586a787b580d
asshurimjoylarita <asshurim@cjnetsolutions.com>
2014-12-08 14:37:43 +0800

    Fetch Options

--- app/controllers/PaymentResponseController.php
+++ app/controllers/PaymentResponseController.php
@@ -11,14 +11,22 @@
 
 		public function handle()
 		{
+//			Create DB Model
 			PaymentResponse::create( [
 
-				'data'     => serialize( Input::all() ),
-				'entry_id' => Input::get( 'entry_id', 0 ),
+				'data'       => serialize( Input::all() ),
+				'entry_id'   => Input::get( 'entry_id', 0 ),
+				'invoice_id' => Input::get( 'invoice_id', 0 ),
 
 			] );
 
-			return;
+//          Redirect TO Success page if has an entry_id
+			if ( Input::has( 'entry_id' ) )
+			{
+				return Redirect::to( WPOptions::fetch()->siteurl . '/success-payments' );
+			}
+
+			return true;
 		}
 
 
