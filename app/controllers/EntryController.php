a5bb8342cb173aee70834cb7863990af74e37aa7
asshurimjoylarita <asshurim@cjnetsolutions.com>
2014-12-09 15:24:10 +0800

    Datatables

--- app/controllers/EntryController.php
+++ app/controllers/EntryController.php
@@ -16,8 +16,10 @@
 //			dd($online_entries->toArray());
 //			$free_entries = LeadDetails::where( 'value', '2checkout' )->join('wp_rg_lead_detail')->get();
 
-			$free_entries = LeadDetails::where('value', 'like', 'Free%')->get();
+			$free_entries = LeadDetails::where('value', 'like', 'Free%')->groupBy('lead_id')->get();
 			$free_entries->load('lead_details');
+
+//			dd($free_entries->toArray());
 
 			$bank_entries = LeadDetails::where('value', 'Bank Account')->get();
 			$bank_entries->load('lead_details');
